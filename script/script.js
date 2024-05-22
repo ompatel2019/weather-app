4eeb95a181964e641ee0af02b00ed59f16f0fd12
ompatel2019 <149570614+ompatel2019@users.noreply.github.com>
2024-05-22 23:57:35 +1000

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,106 @@
+var locationElem = document.getElementById('location');
+var tempElem = document.getElementById('temp');
+var weatherDescElem = document.getElementById('weather-desc');
+var humidityElem = document.getElementById('humidity');
+var sunriseElem = document.getElementById('sunrise');
+var sunsetElem = document.getElementById('sunset');
+var precipitationElem = document.getElementById('rain');
+var windElem = document.getElementById('wind');
+
+
+
+function commenceWeatherCheck() {
+    const cityNameElement = document.getElementById('search-id');
+    const cityName = cityNameElement.value.trim();
+
+    cityNameElement.value = '';
+
+    if (cityName) {
+        const cityNameStr = String(cityName);
+        checkWeather(cityNameStr);
+    } else {
+        console.log('City name is empty');
+    }
+
+}
+
+
+function setWeatherBg(weatherId, bodyElement) {
+    bodyElement.classList = '';
+
+    if (weatherId === 'Clear') {
+        bodyElement.classList.add('bg-sunny-grad');
+    } else if (weatherId === 'Clouds') {
+        bodyElement.classList.add('bg-cloudy-grad');
+    } else if (weatherId === 'Haze') {
+        bodyElement.classList.add('bg-haze-grad');
+    } else if (weatherId == 'Rain') {
+        bodyElement.classList.add('bg-rainy-grad');
+    } else if (weatherId === 'Thunderstorm') {
+        bodyElement.classList.add('bg-thunderstorm-grad');
+    } else if (weatherId == 'Smoke') {
+        bodyElement.classList.add('bg-smoke-grad');
+    } else if (weatherId === 'Mist') {
+        bodyElement.classList.add('bg-mist-grad');
+    }
+}
+
+async function checkWeather(city) {
+    const apiKey = '0dcc20e2360937ef6479712681689681';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}`;
+    const bodyElement = document.getElementById('body');
+
+    
+    try {
+        const response = await fetch(apiUrl + `&appid=${apiKey}`);
+        const data = await response.json();
+        
+        const temp = data.main.temp;
+        const humidity = data.main.humidity;
+        const wind = data.wind.speed;
+        const weatherId = data.weather[0].main;
+    
+        const weatherDescription = data.weather[0].description;
+        let precipitation = '0mm';
+    
+        if (data.rain && data.rain['1h']) {
+            precipitation = data.rain['1h'];
+        }
+    
+        // Extract sunrise and sunset times
+        const roundToNearestMinute = (timestamp) => {
+            const date = new Date(timestamp * 1000);
+            date.setSeconds(0, 0); 
+            return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        };
+    
+        const sunrise = roundToNearestMinute(data.sys.sunrise);
+        const sunset = roundToNearestMinute(data.sys.sunset);
+
+        if (data.cod === '404') {
+            alert('City not found.');
+            return;
+        }
+
+        setWeatherBg(weatherId, bodyElement);
+
+        locationElem.innerHTML = `${data.name}`;
+        tempElem.innerHTML = `${temp} &deg;`;
+        weatherDescElem.innerHTML = `${weatherDescription}`;
+        humidityElem.innerHTML = `${humidity}`;
+
+        sunriseElem.innerHTML = `${sunrise}`;
+        sunsetElem.innerHTML = `${sunset}`;
+        precipitationElem.innerHTML = `${weatherDescription}`;
+        windElem.innerHTML = `${wind} km/hr`;
+
+        if (precipitation === '0mm') {
+            precipitationElem.innerHTML = '0mm';
+        } else {
+            precipitationElem.innerHTML = `${weatherDescription}`;
+        }
+        
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./*.html", "./script/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'startGrad': 'linear-gradient(to bottom, #89CFF0, #D8BFD8)',
+        'sunny-grad': 'linear-gradient(to bottom, #5DBACB, #E4E3C3)',
+        'cloudy-grad': 'linear-gradient(to bottom, #D3D3D3, #A9A9A9)', 
+        'haze-grad': 'linear-gradient(to bottom, #B0C4DE, #696969)', 
+        'rainy-grad': 'linear-gradient(to bottom, #00C6FF, #0072FF)', 
+        'thunderstorm-grad': 'linear-gradient(to bottom, #283E51, #4B79A1)', 
+        'smoke-grad': 'linear-gradient(to bottom, #708090, #2F4F4F)', 
+        'mist-grad': 'linear-gradient(to bottom, #A4A8A9, #E0E4E5)', 
+      },
+      backgroundColor: {
+        'bg-1': '#ebfffc',
+      },
+      fontFamily: {
+        sans: ['Manrope', 'sans-serif'], /* Ensure Manrope Variable is used */
+      }
+    },
   },
   plugins: [],
 }
